c293cb86537911ca815aab35ccde770717c571eb
Hegar Garcia <hegargarcia@gmail.com>
2022-06-13 01:14:51 -0500

    refactor: search for any successful workflow runs

--- find-successful-workflow.js
+++ find-successful-workflow.js
@@ -1,18 +1,22 @@
 const { Octokit } = require("@octokit/action");
 const core = require("@actions/core");
-const github = require('@actions/github');
-const { execSync } = require('child_process');
-const { existsSync } = require('fs');
-const { join } = require('path');
+const github = require("@actions/github");
+const { execSync } = require("child_process");
+const { existsSync } = require("fs");
+const { join } = require("path");
 
-const { runId, repo: { repo, owner }, eventName } = github.context;
+const {
+  runId,
+  repo: { repo, owner },
+  eventName,
+} = github.context;
 process.env.GITHUB_TOKEN = process.argv[2];
 const mainBranchName = process.argv[3];
 const errorOnNoSuccessfulWorkflow = process.argv[4];
 const lastSuccessfulEvent = process.argv[5];
 const workingDirectory = process.argv[6];
 const workflowId = process.argv[7];
-const defaultWorkingDirectory = '.';
+const defaultWorkingDirectory = ".";
 
 let BASE_SHA;
 (async () => {
@@ -20,15 +24,15 @@ let BASE_SHA;
     if (existsSync(workingDirectory)) {
       process.chdir(workingDirectory);
     } else {
-      process.stdout.write('\n');
+      process.stdout.write("\n");
       process.stdout.write(`WARNING: Working directory '${workingDirectory}' doesn't exist.\n`);
     }
   }
 
-  const HEAD_SHA = execSync(`git rev-parse HEAD`, { encoding: 'utf-8' });
+  const HEAD_SHA = execSync(`git rev-parse HEAD`, { encoding: "utf-8" });
 
-  if (eventName === 'pull_request') {
-    BASE_SHA = execSync(`git merge-base origin/${mainBranchName} HEAD`, { encoding: 'utf-8' });
+  if (eventName === "pull_request") {
+    BASE_SHA = execSync(`git merge-base origin/${mainBranchName} HEAD`, { encoding: "utf-8" });
   } else {
     try {
       BASE_SHA = await findSuccessfulCommit(workflowId, runId, owner, repo, mainBranchName, lastSuccessfulEvent);
@@ -38,29 +42,31 @@ let BASE_SHA;
     }
 
     if (!BASE_SHA) {
-      if (errorOnNoSuccessfulWorkflow === 'true') {
+      if (errorOnNoSuccessfulWorkflow === "true") {
         reportFailure(mainBranchName);
         return;
       } else {
-        process.stdout.write('\n');
+        process.stdout.write("\n");
         process.stdout.write(`WARNING: Unable to find a successful workflow run on 'origin/${mainBranchName}'\n`);
         process.stdout.write(`We are therefore defaulting to use HEAD~1 on 'origin/${mainBranchName}'\n`);
-        process.stdout.write('\n');
-        process.stdout.write(`NOTE: You can instead make this a hard error by setting 'error-on-no-successful-workflow' on the action in your workflow.\n`);
+        process.stdout.write("\n");
+        process.stdout.write(
+          `NOTE: You can instead make this a hard error by setting 'error-on-no-successful-workflow' on the action in your workflow.\n`
+        );
 
-        BASE_SHA = execSync(`git rev-parse HEAD~1`, { encoding: 'utf-8' });
-        core.setOutput('noPreviousBuild', 'true');
+        BASE_SHA = execSync(`git rev-parse HEAD~1`, { encoding: "utf-8" });
+        core.setOutput("noPreviousBuild", "true");
       }
     } else {
-      process.stdout.write('\n');
+      process.stdout.write("\n");
       process.stdout.write(`Found the last successful workflow run on 'origin/${mainBranchName}'\n`);
       process.stdout.write(`Commit: ${BASE_SHA}\n`);
     }
   }
 
-  const stripNewLineEndings = sha => sha.replace('\n', '');
-  core.setOutput('base', stripNewLineEndings(BASE_SHA));
-  core.setOutput('head', stripNewLineEndings(HEAD_SHA));
+  const stripNewLineEndings = (sha) => sha.replace("\n", "");
+  core.setOutput("base", stripNewLineEndings(BASE_SHA));
+  core.setOutput("head", stripNewLineEndings(HEAD_SHA));
 })();
 
 function reportFailure(branchName) {
@@ -85,26 +91,28 @@ function reportFailure(branchName) {
 async function findSuccessfulCommit(workflow_id, run_id, owner, repo, branch, lastSuccessfulEvent) {
   const octokit = new Octokit();
   if (!workflow_id) {
-    workflow_id = await octokit.request(`GET /repos/${owner}/${repo}/actions/runs/${run_id}`, {
-      owner,
-      repo,
-      branch,
-      run_id
-    }).then(({ data: { workflow_id } }) => workflow_id);
-    process.stdout.write('\n');
+    workflow_id = await octokit
+      .request(`GET /repos/${owner}/${repo}/actions/runs/${run_id}`, {
+        owner,
+        repo,
+        branch,
+        run_id,
+      })
+      .then(({ data: { workflow_id } }) => workflow_id);
+    process.stdout.write("\n");
     process.stdout.write(`Workflow Id not provided. Using workflow '${workflow_id}'\n`);
   }
   // fetch all workflow runs on a given repo/branch/workflow with push and success
-  const shas = await octokit.request(`GET /repos/${owner}/${repo}/actions/workflows/${workflow_id}/runs`, {
-    owner,
-    repo,
-    branch,
-    workflow_id,
-    event: lastSuccessfulEvent,
-    status: 'success'
-  }).then(({ data: { workflow_runs } }) => workflow_runs.map(run => run.head_sha));
+  const shas = await octokit
+    .request(`GET /repos/${owner}/${repo}/actions/workflows/${workflow_id}/runs`, {
+      owner,
+      repo,
+      workflow_id,
+      status: "success",
+    })
+    .then(({ data: { workflow_runs } }) => workflow_runs.map((run) => run.head_sha));
 
-  return await findExistingCommit(shas);
+  return findExistingCommit(shas);
 }
 
 /**
@@ -112,9 +120,9 @@ async function findSuccessfulCommit(workflow_id, run_id, owner, repo, branch, la
  * @param {string[]} commit_shas
  * @returns {string?}
  */
-async function findExistingCommit(shas) {
+function findExistingCommit(shas) {
   for (const commitSha of shas) {
-    if (await commitExists(commitSha)) {
+    if (commitExists(commitSha)) {
       return commitSha;
     }
   }
@@ -126,9 +134,9 @@ async function findExistingCommit(shas) {
  * @param {string} commitSha
  * @returns {boolean}
  */
-async function commitExists(commitSha) {
+function commitExists(commitSha) {
   try {
-    execSync(`git cat-file -e ${commitSha}`, { stdio: ['pipe', 'pipe', null] });
+    execSync(`git cat-file -e ${commitSha}`, { stdio: ["pipe", "pipe", null] });
     return true;
   } catch {
     return false;
